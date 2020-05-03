5c0c1eb67486297e00fe6337f26568c5fe4d9dd6
magnificul <yoroadyes@gmail.com>
2020-05-03 12:04:02 +0300

    Am terminat Exercitiul 4

--- Exercitiul4/src/Main.java
+++ Exercitiul4/src/Main.java
@@ -4,4 +4,35 @@ public class Main {
     //Se da un text
     //A. Sa se numere toate cuvintele care au lungimea mai mare decat 2, contin caracterul "i", si caracterul "i" se afla pe o pozitie para.
     //Matei
+
+    public static void main(String[] args) {
+
+        String text = "Obiectul istoriei nu se limitează doar la evenimentele politice, cum ar fi de exemplu bătăliile sau faptele conducătorilor.";
+
+
+         int numarCuvinte = numereCuvinte(text);
+        System.out.println("Numarul de cuvinte este: " + numarCuvinte);
+
+    }
+
+    public static int numereCuvinte(String text){
+
+        int numarCuvinte = 0;
+
+        String[] cuvinte = text.split(" ");
+
+        for(int cursor = 0; cursor < cuvinte.length; cursor ++ ){
+            if(cuvinte[cursor].length() > 2 && cuvinte[cursor].contains("i") && cuvinte[cursor].indexOf("i") % 2 == 0){
+                numarCuvinte++;
+            }
+
+        }
+
+        return numarCuvinte;
+
+    }
+
 }
+
+
+
