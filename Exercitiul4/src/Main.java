559b6b13d13e24354ebaae141d505481b72c52f5
magnificul <yoroadyes@gmail.com>
2020-05-03 11:46:31 +0300

    Exercitiul 4 neterminat

---
+++ Exercitiul4/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+
+    //Cerinta:
+    //Se da un text
+    //A. Sa se numere toate cuvintele care au lungimea mai mare decat 2, contin caracterul "i", si caracterul "i" se afla pe o pozitie para.
+    //Matei
+}
